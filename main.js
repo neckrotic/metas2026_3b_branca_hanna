105e3435a941922ca5649ec34fb2975c68c6dfbe
neckrotic <franca.hanna@escola.pr.gov.br>
2026-06-15 11:28:42 -0300

    nhee

--- main.js
+++ main.js
@@ -1,55 +1,35 @@
 const botoes = document.querySelectorAll(".botao");
 const textos = document.querySelectorAll(".aba-conteudo");
 
-for (let i = 0 < botoes.length; i++){
+// ===== ABAS =====
+for (let i = 0; i < botoes.length; i++) {
     botoes[i].onclick = function () {
-        for (let j = 0; j <botoes.length; j++){
-        botoes[j].classList.remove("ativo");
-        textos[j].classList.remove("ativo");
+
+        for (let j = 0; j < botoes.length; j++) {
+            botoes[j].classList.remove("botao-ativo");
+            textos[j].classList.remove("aba-conteudo-ativo");
         }
-        botoes[i].classList.add("ativo");
-        textos[i].classList.add("ativo");
-    }
+
+        botoes[i].classList.add("botao-ativo");
+        textos[i].classList.add("aba-conteudo-ativo");
+    };
 }
 
-const contadores = document.querySelectorAll(".contador");
-const tempoObjetivo1 = new Date(2026-11-01T00:00:00)
-const tempoObjetivo2 = new Date(2026-12-18T00:00:00)
-const tempoObjetivo3 = new Date(2026-12-20T00:00:00)
-const tempoObjetivo4 = new Date(2027-02-01T00:00:00)
+// ===== CONTADOR =====
+const tempoObjetivo1 = new Date("2026-11-01T00:00:00");
+const tempoObjetivo2 = new Date("2026-12-18T00:00:00");
+const tempoObjetivo3 = new Date("2026-12-20T00:00:00");
+const tempoObjetivo4 = new Date("2027-02-01T00:00:00");
 
-const tempos = [tempoObjetivo1, tempoObjetivo2, tempoObjetivo3, tempoObjetivo4]
+const tempos = [
+    tempoObjetivo1,
+    tempoObjetivo2,
+    tempoObjetivo3,
+    tempoObjetivo4
+];
 
-
-assignment
-Atividade: "Projeto 1 - T2 Finalização do projeto do 1º Tri"
-Haward Antunny da Silva Americo postou uma nova atividade: Projeto 1 - T2 Finalização do projeto do 1º Tri
-Criado em: 12:1512:15
-
-Postada por Haward Antunny da Silva Americo
-Haward Antunny da Silva Americo
-Criado em: 12:0512:05
-function calculaTempo(tempoObjetivo) {
-    let tempoAtual = new Date();
-    let tempoFinal = tempoObjetivo - tempoAtual;
-    let segundos = Math.floor(tempoFinal / 1000);
-    let minutos = Math.floor(segundos / 60);
-    let horas = Math.floor(minutos / 60);
-    let dias = Math.floor(horas / 24);
-
-    segundos %= 60;
-    minutos %= 60;
-    horas %= 24;
-    if (tempoFinal > 0){
-        return dias + " dias " + horas + " horas " + minutos + " minutos " + segundos + " segundos";
-    } else {
-        return "Prazo Finalizado";
-    }
-}
-
-function atualizaCronometro(){
-
-    for (let i = 0; i < tempos.length; i++){
+function atualizaCronometro() {
+    for (let i = 0; i < tempos.length; i++) {
 
         let tempoAtual = new Date();
         let tempoFinal = tempos[i] - tempoAtual;
@@ -63,7 +43,7 @@ function atualizaCronometro(){
         minutos %= 60;
         horas %= 24;
 
-        if (tempoFinal > 0){
+        if (tempoFinal > 0) {
             document.getElementById(`dias${i}`).textContent = dias;
             document.getElementById(`horas${i}`).textContent = horas;
             document.getElementById(`min${i}`).textContent = minutos;
@@ -77,9 +57,9 @@ function atualizaCronometro(){
     }
 }
 
-function comecaCronometro(){
+function comecaCronometro() {
     atualizaCronometro();
-    setInterval(atualizaCronometro,1000);
+    setInterval(atualizaCronometro, 1000);
 }
 
 comecaCronometro();
